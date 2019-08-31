91c2d44ba49c2bdc137ad5ed876271ee4141b9e8
Daniel Costa <dpcosta@gmail.com>
2019-08-31 16:46:51 -0300

    Projeto inicial

---
+++ src/Helper/EntityManagerCreator.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Alura\Doctrine\Helper;
+
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Tools\Setup;
+
+class EntityManagerCreator
+{
+    public function criaEntityManager(): EntityManagerInterface
+    {
+        $config = Setup::createXMLMetadataConfiguration(
+            [__DIR__ . '/../../mapeamentos']
+        );
+        $con = [
+            'driver' => 'pdo_pgsql',
+            'host' => 'localhost',
+            'dbname' => 'alura_filmes',
+            'user' => 'postgres',
+            'password' => 'senhalura',
+        ];
+
+        return EntityManager::create($con, $config);
+    }
+}
