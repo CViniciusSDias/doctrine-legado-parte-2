32ba9c44014a1027e93d5f6ca249be9184961dcf
Vinicius Dias <carlosv775@gmail.com>
2019-09-07 12:51:13 -0300

    Video 4.3
    Falando sobre Native Query

--- src/Repository/RepositorioAtores.php
+++ src/Repository/RepositorioAtores.php
@@ -2,6 +2,7 @@
 
 namespace Alura\Doctrine\Repository;
 
+use Alura\Doctrine\Entity\Ator;
 use Doctrine\ORM\EntityRepository;
 use Doctrine\ORM\Query\ResultSetMapping;
 
@@ -28,6 +29,20 @@ class RepositorioAtores extends EntityRepository
         $rsm = new ResultSetMapping();
         $rsm->addScalarResult('nome', 'nome');
         $rsm->addScalarResult('qtd_filmes', 'qtdFilmes');
+
+        return $this->getEntityManager()
+            ->createNativeQuery($sql, $rsm)
+            ->getResult();
+    }
+
+    public function buscaTodosAtores()
+    {
+        $sql = 'SELECT id_ator, primeiro_nome, ultimo_nome FROM ator;';
+        $rsm = new ResultSetMapping();
+        $rsm->addEntityResult(Ator::class, 'ator');
+        $rsm->addFieldResult('ator', 'id_ator', 'id');
+        $rsm->addFieldResult('ator', 'primeiro_nome', 'primeiroNome');
+        $rsm->addFieldResult('ator', 'ultimo_nome', 'ultimoNome');
 
         return $this->getEntityManager()
             ->createNativeQuery($sql, $rsm)
